5d028424eca71a0e25eb0f68b320c717816b541a
jp-reynolds <jpreynolds215@gmail.com>
2018-04-02 20:29:35 -0600

    Commit 5: Made squares into circles, mapped out future game logic

---
+++ project1.js
@@ -0,0 +1,10 @@
+//things I want to do...
+
+//make a timer of 15 seconds for level 1
+//make a timer of 30 seconds for level 2
+//make a timer of 60 seconds for level 3
+
+//when timer is up announce that a life is lost and to try again.
+
+//if a player moves outside of the red cells subtract 1 player life (10 to start)
+//when the player hovers over the finish square make a level passed for 1 and 2 and 'game won' for level 3.
