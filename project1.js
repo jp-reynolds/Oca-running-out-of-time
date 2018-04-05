8f96d3992ad4484a81bcf472118a24e52b3355a9
jp-reynolds <jpreynolds215@gmail.com>
2018-04-05 16:13:46 -0600

    Commit 8: when start button pressed...start timer, allow correctPath hover to work

--- project1.js
+++ project1.js
@@ -11,6 +11,35 @@
 
 
 //this jquery will permanently add hover class after hover
- $(".correctPath").on('mouseover', function () {
-     $(this).addClass("hover");
- });
+$(function() { 
+	//when the mouse moves over the space it changes the tile to green permanently
+	//when the start button is clicked it will start the countdown
+	$('#startButton').click(function() {
+		//since this is in the #startButton click function it won't work until the start button is clicked
+		//when the mouse hovers over correctPath tiles it turns them green
+		$(".correctPath").on('mouseover', function () {
+ 			$(this).addClass("hover");
+ 		});
+
+
+	//timer starts at this #
+	var sec = 3;
+	//setting a timer variable that communicates with the stopwatch decreasing by one second
+	var timer = setInterval(function () {
+		sec--;
+		$("#stopwatch").html(sec);
+		//conditional statement, if seconds is 0 stop counting 
+		if (sec === 0) {
+			clearInterval(timer);
+			alert("Out of time, GAME OVER");
+		}
+	//if the seconds are above 0, keep decreasing	
+	}, 1000);
+	});
+
+	
+
+});
+
+
+
