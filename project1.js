21508a2360236d6bd1f8ba95ddad179a0bad4e65
jp-reynolds <jpreynolds215@gmail.com>
2018-04-07 01:11:24 -0600

    Commit 11: 1/3 of maze travel complete. introduced treasure/key

--- project1.js
+++ project1.js
@@ -11,55 +11,153 @@ $(function() {
  				
 //this reveals the appropriate colors for warning and correctPath.
 		//row 5 column 1
-		$( ".tile1" ).mouseover(function() {
+		$( ".r6c1" ).mouseover(function() {
 			$('.reveal1.correctPath').addClass('correctPathHover');
 			$('.reveal1.warning').addClass('warningHover');
 		});
 		//row 5 column 2
-		$( ".tile2" ).mouseover(function() {
+		$( ".r6c2" ).mouseover(function() {
 			$('.reveal2.correctPath').addClass('correctPathHover');
 			$('.reveal2.warning').addClass('warningHover');
 		});
 		//row 5 column 3
-		$( ".tile3" ).mouseover(function() {
+		$( ".r6c3" ).mouseover(function() {
 			$('.reveal3.correctPath').addClass('correctPathHover');
 			$('.reveal3.warning').addClass('warningHover');
 		});
 		//row 5 column 4
-		$( ".tile4" ).mouseover(function() {
+		$( ".r6c4" ).mouseover(function() {
 			$('.reveal4.correctPath').addClass('correctPathHover');
 			$('.reveal4.warning').addClass('warningHover');
 		});
 		//row 5 column 5
-		$( ".tile5" ).mouseover(function() {
+		$( ".r6c5" ).mouseover(function() {
 			$('.reveal5.correctPath').addClass('correctPathHover');
 			$('.reveal5.warning').addClass('warningHover');
 		});
 
-		$( ".tile6" ).mouseover(function() {
+		$( ".r6c6" ).mouseover(function() {
 			$('.reveal6.correctPath').addClass('correctPathHover');
 			$('.reveal6.warning').addClass('warningHover');
 		});
 
-		$( ".tile7" ).mouseover(function() {
+		$( ".r6c7" ).mouseover(function() {
 			$('.reveal7.correctPath').addClass('correctPathHover');
 			$('.reveal7.warning').addClass('warningHover');
 		});
 
-		$( ".tile8" ).mouseover(function() {
+		$( ".r6c8" ).mouseover(function() {
 			$('.reveal8.correctPath').addClass('correctPathHover');
 			$('.reveal8.warning').addClass('warningHover');
 		});
 
-		$( ".tile9" ).mouseover(function() {
+		$( ".r6c9" ).mouseover(function() {
 			$('.reveal9.correctPath').addClass('correctPathHover');
 			$('.reveal9.warning').addClass('warningHover');
 		});
 
-		$( ".tile10" ).mouseover(function() {
+		$( ".r6c10" ).mouseover(function() {
 			$('.reveal10.correctPath').addClass('correctPathHover');
 			$('.reveal10.warning').addClass('warningHover');
 		});
+		//TURN UP
+		$( ".r5c3" ).mouseover(function() {
+			$('.reveal11.correctPath').addClass('correctPathHover');
+			$('.reveal11.warning').addClass('warningHover');
+		});
+
+		$( ".r4c3" ).mouseover(function() {
+			$('.reveal12.correctPath').addClass('correctPathHover');
+			$('.reveal12.warning').addClass('warningHover');
+		});
+
+		$( ".r3c3" ).mouseover(function() {
+			$('.reveal13.correctPath').addClass('correctPathHover');
+			$('.reveal13.warning').addClass('warningHover');
+		});
+		//TOP LEFT CORNER
+		$( ".r2c2" ).mouseover(function() {
+			$('.reveal14.correctPath').addClass('correctPathHover');
+			$('.reveal14.warning').addClass('warningHover');
+		});
+
+		$( ".r2c4" ).mouseover(function() {
+			$('.reveal15.correctPath').addClass('correctPathHover');
+			$('.reveal15.warning').addClass('warningHover');
+		});
+
+		$( ".r2c5" ).mouseover(function() {
+			$('.reveal16.correctPath').addClass('correctPathHover');
+			$('.reveal16.warning').addClass('warningHover');
+		});
+
+		$( ".r2c6" ).mouseover(function() {
+			$('.reveal17.correctPath').addClass('correctPathHover');
+			$('.reveal17.warning').addClass('warningHover');
+		});
+
+		$( ".r2c7" ).mouseover(function() {
+			$('.reveal18.correctPath').addClass('correctPathHover');
+			$('.reveal18.warning').addClass('warningHover');
+		});
+
+		$( ".r2c8" ).mouseover(function() {
+			$('.reveal19.correctPath').addClass('correctPathHover');
+			$('.reveal19.warning').addClass('warningHover');
+		});
+
+		$( ".r2c9" ).mouseover(function() {
+			$('.reveal20.correctPath').addClass('correctPathHover');
+			$('.reveal20.warning').addClass('warningHover');
+		});
+
+		$( ".r2c10" ).mouseover(function() {
+			$('.reveal21.correctPath').addClass('correctPathHover');
+			$('.reveal21.warning').addClass('warningHover');
+		});
+
+		$( ".r2c11" ).mouseover(function() {
+			$('.reveal22.correctPath').addClass('correctPathHover');
+			$('.reveal22.warning').addClass('warningHover');
+		});
+
+		$( ".r2c12" ).mouseover(function() {
+			$('.reveal23.correctPath').addClass('correctPathHover');
+			$('.reveal23.warning').addClass('warningHover');
+		});
+
+		$( ".r2c13" ).mouseover(function() {
+			$('.reveal24.correctPath').addClass('correctPathHover');
+			$('.reveal24.warning').addClass('warningHover');
+		});
+
+		$( ".r2c14" ).mouseover(function() {
+			$('.reveal25.correctPath').addClass('correctPathHover');
+			$('.reveal25.warning').addClass('warningHover');
+		});
+
+		$( ".r2c15" ).mouseover(function() {
+			$('.reveal26.correctPath').addClass('correctPathHover');
+			$('.reveal26.warning').addClass('warningHover');
+		});
+
+		$( ".r2c3" ).mouseover(function() {
+			$('.revealtoprow.correctPath').addClass('correctPathHover');
+			$('.revealtoprow.warning').addClass('warningHover');
+		});
+
+		$( ".r7c6" ).mouseover(function() {
+			$('.reveal27.correctPath').addClass('correctPathHover');
+			$('.reveal27.warning').addClass('warningHover');
+		});
+
+		$( ".r3c7" ).mouseover(function() {
+			$('.reveal28.correctPath').addClass('treasure');
+			$('.reveal28.warning').addClass('warningHover');
+		});
+
+
+
 
 
 
